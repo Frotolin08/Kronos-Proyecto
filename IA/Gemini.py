44a67d8c7ddcd934f1c9e8212b7f40bd8fe66987
Julian-Mirkin <49548508@est.ort.edu.ar>
2025-08-20 14:30:08 -0300

    cambios en formato de tabla IA

--- IA/Gemini.py
+++ IA/Gemini.py
@@ -1,4 +1,4 @@
-# pip install -r IA/requirements.txt
+# pip install -r IA/lib.txt
 import os
 import json
 from google import genai
@@ -8,6 +8,7 @@ from io import BytesIO
 from pydantic import BaseModel
 from typing import List
 import base64
+import pandas as pd
 
 client = genai.Client(api_key="")
 
@@ -65,16 +66,23 @@ def createJson(prompt):
             "response_schema": Board
         },
     )
+
     board: Board = response.parsed
     board_dict = {
         "columns": board.columns,
         "rows": board.rows
     }
+
     json_path = os.path.join(SAVE_DIR, "tablita.json")
     with open(json_path, "w", encoding="utf-8") as f:
         json.dump(board_dict, f, ensure_ascii=False, indent=4)
     print("json creado")
 
+    df = pd.DataFrame(board.rows, columns=board.columns)
+    csv_path = os.path.join(SAVE_DIR, "tablita.csv")
+    df.to_csv(csv_path, index=False, encoding="utf-8-sig")
+    print("csv creado")
+
 createJson(
-    "Genera una tabla JSON con exactamente las siguientes columnas: Sitio Web, Tipografía, Colores, Formal o informal, Personajes-iconos-emblemas, Accesibilidad, Capacidad de navegación, Organización (botones importantes), Funciones extras, Tutoriales o instrucciones. Incluye una fila para Mercado Libre, una para Amazon, una para PedidoYa y otra para lo que puedes analizar de la img que te pase en el content. Asegúrate de que cada fila tenga exactamente 11 valores (contando el nombre), uno por cada columna. Cada tabla debe ser descriptiva y desarrollada, teniendo aprox 10 palabras"
+    "Genera una tabla JSON con exactamente las siguientes columnas: Sitio Web, Tipografía, Colores, Formal o informal, Personajes-iconos-emblemas, Accesibilidad, Capacidad de navegación, Organización (botones importantes), Funciones extras, Tutoriales o instrucciones. Incluye una fila para Mercado Libre, una para Amazon, una para PedidoYa y otra para lo que puedes analizar de la img que te pase en el content. Asegúrate de que hayan 10 filas, uno por cada columna. cada fila tendrá 5 columnas (4 para cada uno de los nombres de las páginas) y la última será con topico de Conclusion:. En este pondras que puedes observar que funciona bien en las primeras 3 páginas, y dirás que se puede mejorar de la que te pasé con la img. Debes poner conclusiones especificas por cada topico, no una general. Cada tabla debe ser descriptiva y desarrollada, teniendo aprox 15 palabras"
 )
