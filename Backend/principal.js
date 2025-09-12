1ee0f74efc29c1bb8c0a8c5f6ae851ec41a3189f
Sofffisab <166829864+Sofffisab@users.noreply.github.com>
2025-09-12 08:48:36 -0300

    arreglando consistencia

--- Backend/principal.js
+++ Backend/principal.js
@@ -1,40 +1,34 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
 import dotenv from 'dotenv';
-import argon2 from 'argon2';
-import jwt from 'jsonwebtoken';
+import expressWs from 'express-ws';
+import setuprouter from './rutas.js';
 import setupsesiones from './sesiones/sesiones.js';
 import setupcalendario from './calendario.js';
 import setupautenticacion from './autenticacion.js';
-import setuprouter from './rutas.js';
 import setuparchivos from './archivos/archivos.js';
 import setupwebsocketserver from './chats/websocket.js';
 import setupchat from './chats/chat.js';
 // import setupprojects from './proyectos/proyectos.js';
 // import setuppersonalize from './personalizaciones/personalizaciones.js';
 
-const {authorization, getatoken, lookfortoken, permision, getevents, redirectwithgoogle, createevents, deleteevents, updateevents} = setupcalendario();
-const { login, signup } = setupsesiones(JWT_SECRET);
-const { autenticacion } = setupautenticacion(JWT_SECRET);
-const {seefile, uploadfile} = setuparchivos();
-const { websocketserver } = setupwebsocketserver();
-const router = setuprouter({ login, signup, authentication, getevents, permision, redirectwithgoogle, createevents, deleteevents, updateevents, seefile, uploadfile, websocketserver});
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET || 'contrasenia-jeje';
+const prisma = new PrismaClient();
 const { app, getWss } = expressWs(express()); 
 const wss = getWss(); 
 
-dotenv.config();
-const prisma = new PrismaClient();
-const JWT_SECRET = process.env.JWT_SECRET || 'contrasenia-jeje';
+const { login, signup } = setupsesiones(JWT_SECRET);
+const { authentication } = setupautenticacion(JWT_SECRET);
+const { authorization, getatoken, lookfortoken, permision, getevents, redirectwithgoogle, createevents, deleteevents, updateevents } = setupcalendario();
+const { seefile, uploadfile } = setuparchivos();
+const router = setuprouter({ login, signup, authentication, getevents, permision, redirectwithgoogle, createevents, deleteevents, updateevents, seefile, uploadfile });
+
 app.use(express.json());
 app.use(router);
 
 setupwebsocketserver(wss);
-
-router.ws('/chat/:chatId', (ws, req) => {
-  ws.on('message', (msg) => {
-    // mas adelante, mandar mensajes
-  });
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
