f90abc670e2a67c4bda3c94965dc739dc21d47b0
Mathias <mathifogo@gmail.com>
2025-09-15 14:26:09 -0300

    apenas comence con la project home page
    no hice nada este bloque

--- Frontend/src/Pages/ProjectPage.jsx
+++ Frontend/src/Pages/ProjectPage.jsx
@@ -1,24 +1,12 @@
 import { useParams } from "react-router";
-import SimpleList from "../components/SimpleList";
+import Navbar from "../components/NavBar";
+import ErrorPage from "./ErrorPage";
 
 
 export default function(props) {
 
 const params = useParams();
-let fetch 
-if(params.id=='1') {
-  fetch  = {title: 'pokeort', 
-        members: ['sabio','pipa','pirolo','marabi'],
-    subtitle: 'pokemon pero en ort'
-    }
-}
-const listContent = fetch && fetch.members.map((member) => <li key={member}>{member}</li>)
 return(
-<>
-<h1 className="title">{fetch? fetch.title: 'title' }</h1>
-<h2 className="subtitle">{fetch? fetch.subtitle: 'subtitle' }</h2>
-<SimpleList listTitle='Members' listContent={listContent}/>
-</>
+params.id? <Navbar/> : <ErrorPage/>
 )
-
 }
